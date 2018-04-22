3cc76fc94ea39112f3c6d9c044507f4e7f082120
Lafon <arnaud_lafon@hotmail.fr>
2018-04-22 22:06:37 +0200

    list completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const PastebinAPI = require('pastebin-js');
+const moment = require('moment');
 const { API_KEY, API_USER_NAME, API_USER_PASSWORD } = require('./config.js');
 const { File, Key, Request } = require('./models');
 
@@ -62,10 +63,11 @@ if (key.options.post) {
 
 else if (key.options.download) {
 
+    const pasteId = key.options.download;
+    const dir = key.options.args[0];
+    const req = new Request();
+
     if (key.options.download && key.options.args.length) {
-        const pasteId = key.options.download;
-        const dir = key.options.args[0];
-        const req = new Request();
 
         async function getPastebin() {
             try {
@@ -77,25 +79,37 @@ else if (key.options.download) {
                 console.error(e.message);
             }
         }
+
         getPastebin();
     }
 
     else {
-        console.log(`ERROR: No args found. You need to specify a path. (EX: ./save)`)
+        console.log(`ERROR: No args found. You need to specify a path. (EX: pastebin -d ${pasteId} ./save)`)
     }
 }
 
 else if (key.options.list) {
     const req = new Request();
-
     async function getUserPastes() {
         try {
-            const pastes = req.getUserPastes(pastebin);
-            if (!pastes.length) {
-                console.log("ERROR: You have no pastebin.");
+            const pastes = await req.getUserPastes(pastebin);
+            const pastesL = pastes.length;
+
+            if (!pastesL) {
+                console.log("ERROR: You have no pastebin yet.");
             }
             else {
+                console.log(`You have ${pastesL} pastebin` + (pastesL > 1 ? 's' : ''));
                 console.log(pastes);
+                for (let i = 0; i < pastesL; i++) {
+
+                    const title = pastes[i].paste_title;
+                    const url = pastes[i].paste_url;
+                    const date = pastes[i].paste_date;
+                    const dateTimeString = moment.unix(date).format("DD-MM-YYYY");
+
+                    console.log((title === '' ? 'Untitled =>' : title + ' =>') + ' ' + url + ' added ' + dateTimeString)
+                }
             }
         }
         catch (e) {
--- models/Key.js
+++ models/Key.js
@@ -3,8 +3,8 @@ const program = require('commander');
 class Key {
 	constructor(arg) {
 		this.options = program.version('0.1.0')
-			.option('-p, --post [filePath]', 'Post a pastebin file')
-			.option('-d, --download [pastebinId] [path]', 'Download a pastebin and put it in a specific path')
+			.option('-p, --post [filePath]', 'Post one or more pastebin files')
+			.option('-d, --download [pastebinId] [path]', 'Download a raw paste data and put it in a specific path')
 			.option('-l, --list', 'List user pastebins')
             .parse(arg);
         this.optionActions()
